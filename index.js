596c876af895bed29f483b81a783e3e30b57bff5
RexT2507 <baptiste.marmier@ynov.com>
2020-04-23 17:28:46 +0200

    [DEV][FIX] Ajout du refresh auto et correction css

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
 const path = require('path');
+
+const reload = require('reload');
+
 const router = express.Router();
 
 router.get("/", function(req, res){
@@ -12,8 +15,15 @@ router.get("/manifest", function(req, res){
 });
 
 app.use('/', router);
+
 app.use(express.static('public'));
-app.use(express.static('views'));
+
+app.use(
+  express.static('views')
+);
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Example app listening on port 3000!')
 });
+
+reload(app);
